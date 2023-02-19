7e9c0887e62d67c6d72dd60cfb1df7e9b5eb8b80
holy-garden <mesalina136@hotmail.com>
2023-02-19 19:46:14 +0100

    holy: expand pieces array to hold all cells in the board

--- board.py
+++ board.py
@@ -9,7 +9,7 @@ class Board:
         self.board_texture = self.sprite_sheet.board_at()
 
         self.pieces: list = []
-        self.board = [
+        self.dummy_board = [
             [5, 4, 3, 1, 2, 3, 4, 5],
             [6, 6, 6, 6, 6, 6, 6, 6],
             [0, 0, 0, 0, 0, 0, 0, 0],
@@ -23,14 +23,18 @@ class Board:
     def initialize_board(self) -> None:
         for i in range(8):
             for j in range(8):
-                n = self.board[i][j]
-                if not n:
-                    continue
+                # get piece id 
+                n = self.dummy_board[i][j]
+                # x coordinates (white pieces)
                 x = abs(n) - 1
+                # y coordinates (1/0 white/black pieces)
                 y = 1 if n > 0 else 0
+                # create piece
                 piece = Piece(pos=(x * 100, y * 100), _id=n)
+                # update the pieces' rects position
                 piece.p_rect.x = j * 100
                 piece.p_rect.y = i * 100
+                # add piece
                 self.pieces.append(piece)
 
     def is_position_empty(self, *, pos: tuple[int]) -> bool:
@@ -39,11 +43,20 @@ class Board:
                 return False
         return True
 
-    def make_move(self, *, index: int, old_pos: tuple[int], new_pos: tuple[int]) -> None:
-        if self.is_valid_move(index=index, old_pos=old_pos, new_pos=new_pos):
-            self.pieces[index].p_rect = pygame.Rect(*new_pos, 100, 100)
+    def make_move(self, *, old_pos: tuple[int], new_pos: tuple[int]) -> None:
+        old_index = (old_pos[1] // 100) * 8 + (old_pos[0] // 100)
+        new_index = (new_pos[1] // 100) * 8 + (new_pos[0] // 100)
+        if self.is_valid_move(old_pos=old_pos, new_pos=new_pos):
+            piece_at_old_pos = self.pieces[old_index]
+            piece_at_new_pos = self.pieces[new_index]
+
+            self.pieces[old_index], self.pieces[new_index] = self.pieces[new_index], self.pieces[old_index]
+            self.pieces[new_index].p_rect = pygame.Rect(*new_pos, 100, 100)
+            # del self.pieces[old_index]
+            # self.pieces[new_index] = piece
+            # self.pieces[new_index].p_rect = pygame.Rect(*new_pos, 100, 100)
         else:
-            self.pieces[index].p_rect = pygame.Rect(*old_pos, 100, 100)
+            self.pieces[old_index].p_rect = pygame.Rect(*old_pos, 100, 100)
 
     def is_within_bounds(self, *, new_pos: tuple[int]) -> bool:
         to_x, to_y = new_pos
@@ -53,9 +66,11 @@ class Board:
             return False
         return True
 
-    def is_valid_move(self, *, index: int, old_pos: tuple[int], new_pos: tuple[int]) -> bool:
+    def is_valid_move(self, *, old_pos: tuple[int], new_pos: tuple[int]) -> bool:
+        old_index = (old_pos[1] // 100) * 8 + (old_pos[0] // 100)
+        new_index = (new_pos[1] // 100) * 8 + (new_pos[0] // 100)
         if self.is_within_bounds(new_pos=new_pos):
-            piece: Piece = self.pieces[index]
+            piece: Piece = self.pieces[old_index]
             if abs(piece._id) == 6 and piece.pawn(old_pos=old_pos, new_pos=new_pos):    return True
             if abs(piece._id) == 4 and piece.knight(old_pos=old_pos, new_pos=new_pos):  return True
             if abs(piece._id) == 3 and piece.bishop(old_pos=old_pos, new_pos=new_pos):  return True
